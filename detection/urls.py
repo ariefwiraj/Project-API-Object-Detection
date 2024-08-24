61c5eb20c7cbabf2df8b5e4beebba44c174814b0
Arief Wira Judithantyo <judithantyo@gmail.com>
2024-08-24 10:51:23 +0700

    Update urls.py

--- detection/urls.py
+++ detection/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ObjectDetectionView, ImageWithAnnotationsView, index_view, ImageListView, DetectionDetailView, ImageWithAnnotationsBoundingBoxView, ImageWithAnnotationsMasksView
+from .views import ObjectDetectionView, ImageWithAnnotationsView, index_view, ImageListView, DetectionDetailView, ImageWithAnnotationsBoundingBoxView, ImageWithAnnotationsMasksView, ObjectDetectionFromEncodedView
 
 
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('detection-detail/', DetectionDetailView.as_view(), name='detection-detail'),
     path('image-with-annotations-bbox/', ImageWithAnnotationsBoundingBoxView.as_view(), name='image-with-annotations-bbox'),
     path('image-with-annotations-masks/', ImageWithAnnotationsMasksView.as_view(), name='image-with-annotations-masks'),
+    path('detect-annotator/<str:image_name>/', ObjectDetectionFromEncodedView.as_view(), name='detect-annotator'),
 ]
