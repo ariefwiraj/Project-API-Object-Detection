4e9d87fe4d75176fb5febd60c10df1c6f0ea497c
Arief Wira Judithantyo <judithantyo@gmail.com>
2024-09-10 18:22:14 +0700

    Update settings.py

--- object_detection_api/settings.py
+++ object_detection_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-klvl2m@u5u28$_47_%c1%&yj2$25_oo&tpd3)&rtr--y%i@^^$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
